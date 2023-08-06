e464c595026a49a45583f3673b4f7d614c667737
Egerd Roman <heizyblessedboy1561@gmail.com>
2023-08-07 00:31:30 +0300

    Update Lesson3

---
+++ Lesson3/Exercise1/E1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите номер месяца (1-12): ");
+int month = int.Parse(Console.ReadLine());
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Это Зима");
+        break;
+
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Это Весна");
+        break;
+
+    case 6: 
+    case 7:
+    case 8:
+        Console.WriteLine("Это Лето");
+        break;
+
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Это Осень");
+        break;
+
+    default:
+        Console.WriteLine("Вы ввели не существующий номер месяца");
+        break;
+
+}
---
+++ Lesson3/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите номер месяца (1-12): ");
+int month = int.Parse(Console.ReadLine());
+
+if (month == 12 || month == 1 || month == 2)
+{
+    Console.WriteLine("Это Зима");
+    return;
+}
+
+if (month >= 3 && month <= 5)
+{
+    Console.WriteLine("Это Весна");
+    return;
+}
+
+if (month >= 6 && month <= 8)
+{
+    Console.WriteLine("Это Лето");
+    return;
+}
+
+if (month >= 9 && month <= 11)
+{
+    Console.WriteLine("Это Осень");
+    return;
+}
+
+Console.WriteLine("Вы ввели не существующий номер месяца");
