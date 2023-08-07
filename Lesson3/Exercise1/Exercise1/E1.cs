e95b0611531274173264cb1968ed7c5c9ba02647
Egerd Roman <heizyblessedboy1561@gmail.com>
2023-08-07 21:25:12 +0300

    Fix lesson3

---
+++ Lesson3/Exercise1/Exercise1/E1.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите номер месяца (1-12): ");
+string strMonth = Console.ReadLine();
+bool tryMonth = int.TryParse(strMonth, out int numMonth);
+Action<string> userOutput = Console.WriteLine;
+
+switch (numMonth)
+{
+    case 12:
+    case 1:
+    case 2:
+        userOutput("Это зима");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        userOutput("Это Весна");
+        break;
+    case 6: 
+    case 7:
+    case 8:
+        userOutput("Это Лето");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        userOutput("Это Осень");
+        break;
+    default:
+        userOutput("Вы ввели не существующий номер месяца");
+        break;
+}
---
+++ Lesson3/Exercise1/Exercise4/E4.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите температуру:");
+var temp = Console.ReadLine();
+Action<string> userOutput = Console.WriteLine;
+
+if (int.TryParse(temp, out int numTemp))
+{
+    if (numTemp > -5)
+    {
+        userOutput("Тепло");
+        return;
+    }
+    if (numTemp >= -20)
+    {
+        userOutput("Нормально");
+        return;
+    }
+    if (numTemp <= -21)
+    {
+        userOutput("Холодно");
+        return;
+    }
+
+}
+
+userOutput("Вы ввели неверное или несуществующее значение");
