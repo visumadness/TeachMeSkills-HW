eaa9dfb2411ee4ab88491e84497deb2eeeecee9c
Egerd Roman <heizyblessedboy1561@gmail.com>
2023-08-19 17:59:17 +0300

    Change Lesson4

---
+++ Lesson4/Task2/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal enum Operation
+    {
+        None,
+        Addition,
+        Substraction,
+        Multiplication,
+        Division,
+    }
+}
---
+++ Lesson4/Task3/Task3.cs
@@ -0,0 +1,32 @@
+﻿const string RandomNumbers = "Array random numbers :";
+const string ValuesAfterBubbleSort = "Bubble sort :";
+
+int[] numbers = new int[12];
+int temp;
+Random random = new Random();
+Console.Write(RandomNumbers);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(1, 25);
+    Console.Write("{0} ", numbers[i]);
+}
+Console.WriteLine();
+Console.Write(ValuesAfterBubbleSort);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = 0; j < numbers.Length - 1 - i; j++)
+    {
+        if (numbers[j] > numbers[j + 1])
+        {
+            temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+        }
+    }
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("{0} ", numbers[i]);
+}
