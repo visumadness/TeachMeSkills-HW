8b657ccece86f2d12cef9179d1d7df04dde6f1ea
Egerd Roman <heizyblessedboy1561@gmail.com>
2023-09-05 22:20:17 +0300

    Create Lesson11

---
+++ Lesson11/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Stack<int> stack = new Stack<int>();
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+
+Console.WriteLine(stack.Pop());
+Console.WriteLine(stack.Pop());
+
+Console.WriteLine(stack.Peek());
+Console.WriteLine(stack.Pop());
---
+++ Lesson11/Task1/Stack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private LinkedList<T> _items = new LinkedList<T>();
+
+        public void Push(T item)
+        {
+            _items.AddLast(item);
+        }
+
+        public T Pop()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T lastItem = _items.Last.Value;
+            _items.RemoveLast();
+            return lastItem;
+        }
+
+        public T Peek()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            return _items.Last.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return _items.Count == 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
